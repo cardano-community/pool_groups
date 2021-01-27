5b859bf6729bbdd24ebfc2b92093ce54d5d78494
rdlrt <3169068+rdlrt@users.noreply.github.com>
2021-01-27 04:53:07 +0000

    Add case for adapools returning as null group, but adastat classifying it as one

--- update_pools.py
+++ update_pools.py
@@ -40,16 +40,22 @@ def main():
       mismatch[str(pool['pool_id'])]={'adapools': str(pool['group'] + " - not in list"),'adastat': None}
   for pool in adastat_js:
     matched=0
-    for ad_pool in adastat_js:
+    isdiff=0
+    for ad_pool in adapools_js:
       if ad_pool['pool_id'] == pool['pool_id']:
         matched=1
+        if ad_pool['group'] == None:
+          isdiff=1
         break
-    if matched == 0:
-      mismatch[str(pool['cluster_name'])]={'adapools': None,'adastat': str(pool['cluster_name'])}
-      pool_info={"pool_id": str(pool['pool_id']), "ticker": str(adapools_list[str(pool['pool_id'])]['db_ticker']), "name": str(adapools_list[str(pool['pool_id'])]['db_name'])}
-      if str(pool['cluster_name']) not in groups:
-        groups[str(pool['cluster_name'])]=[]
-      groups[str(pool['cluster_name'])].append_info(pool_info)
+    if matched == 0 or isdiff == 1:
+      mismatch[str(pool['pool_id'])]={'adapools': None,'adastat': str(pool['cluster_name'])}
+      if str(pool['pool_id']) in adapools_list:
+        pool_info={"pool_id": str(pool['pool_id']), "ticker": str(adapools_list[str(pool['pool_id'])]['db_ticker']), "name": str(adapools_list[str(pool['pool_id'])]['db_name'])}
+        if str(pool['cluster_name']) not in groups:
+          groups[str(pool['cluster_name'])]=[]
+        groups[str(pool['cluster_name'])].append(pool_info)
+      else:
+        mismatch[str(pool['pool_id'])]={'adapools': "None - not in list",'adastat': str(pool['cluster_name'])}
   with open(jsonf, 'w', encoding='utf-8') as jsonfo:
       jsonfo.write(json.dumps(groups, indent=2, sort_keys=True))
   with open(dscrpncyf, 'w', encoding='utf-8') as dscrpncyfo:
